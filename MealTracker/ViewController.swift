23ce78403a99d567258311b38347b66cfc673fdd
Travis Brigman <travis1000@gmail.com>
2018-12-28 09:07:32 -0600

    Initial Commit

---
+++ MealTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MealTracker
+//
+//  Created by Travis Brigman on 12/28/18.
+//  Copyright © 2018 Travis Brigman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
